e91e0ad101d5a64cdc786a125bfde8d475f120de
José Salcedo Uribe <pepoclesng@gmail.com>
2023-11-09 13:22:14 -0600

    test build command

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
     console.log("Here!");
     res.status(200).json({ ok: true, msg: "here" });
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port http://localhost/${port}`);
+    console.log(`Server listening on port http://localhost:${port}`);
 });
